9464e6b9704b9598511f67ce0790844b4b2ec21e
Kaustav Das Modak <kaustav.dasmodak@freshworks.com>
2021-12-23 11:31:59 +0530

    (wip) upgrade freshfoods to GA
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@freshworks.com>

--- freshfoods/app/assets/js/chart.js
+++ freshfoods/app/assets/js/chart.js
@@ -1,100 +1,96 @@
 // Code for creating an SVG donut chart from feature properties
 function createDonutChart(props) {
-    var offsets = [];
-    var counts = [
-        props.fruit,
-        props.milk
-    ];
-    var total = 0;
-    for (var i = 0; i < counts.length; i++) {
-        offsets.push(total);
-        total += counts[i];
-    }
-    var fontSize =
-        total >= 1000 ? 32 : total >= 100 ? 30 : total >= 10 ? 28 : 26;
-    var r = total >= 1000 ? 64 : total >= 100 ? 48 : total >= 10 ? 32 : 24;
-    var r0 = Math.round(r * 0.65);
-    var w = r * 2;
+  var offsets = [];
+  var counts = [props.fruit, props.milk];
+  var total = 0;
+  for (var i = 0; i < counts.length; i++) {
+    offsets.push(total);
+    total += counts[i];
+  }
+  var fontSize = total >= 1000 ? 32 : total >= 100 ? 30 : total >= 10 ? 28 : 26;
+  var r = total >= 1000 ? 64 : total >= 100 ? 48 : total >= 10 ? 32 : 24;
+  var r0 = Math.round(r * 0.65);
+  var w = r * 2;
 
-    var html =
-        '<div><svg width="' +
-        w +
-        '" height="' +
-        w +
-        '" viewbox="0 0 ' +
-        w +
-        ' ' +
-        w +
-        '" text-anchor="middle" style="font: ' +
-        fontSize +
-        'px Bebas Neue, sans-serif; display: block">';
+  var html =
+    '<div><svg width="' +
+    w +
+    '" height="' +
+    w +
+    '" viewbox="0 0 ' +
+    w +
+    " " +
+    w +
+    '" text-anchor="middle" style="font: ' +
+    fontSize +
+    'px Bebas Neue, sans-serif; display: block">';
 
-    for (i = 0; i < counts.length; i++) {
-        html += donutSegment(
-            offsets[i] / total,
-            (offsets[i] + counts[i]) / total,
-            r,
-            r0,
-            colors[i]
-        );
-    }
-    html +=
-        '<circle cx="' +
-        r +
-        '" cy="' +
-        r +
-        '" r="' +
-        r0 +
-        '" fill="#ffffffaa" /><text dominant-baseline="central" transform="translate(' +
-        r +
-        ', ' +
-        r +
-        ')">' +
-        total.toLocaleString() +
-        '</text></svg></div>';
+  for (i = 0; i < counts.length; i++) {
+    html += donutSegment(
+      offsets[i] / total,
+      (offsets[i] + counts[i]) / total,
+      r,
+      r0,
+      colors[i]
+    );
+  }
+  html +=
+    '<circle cx="' +
+    r +
+    '" cy="' +
+    r +
+    '" r="' +
+    r0 +
+    '" fill="#ffffffaa" /><text dominant-baseline="central" transform="translate(' +
+    r +
+    ", " +
+    r +
+    ')">' +
+    total.toLocaleString() +
+    "</text></svg></div>";
 
-    var el = document.createElement('div');
-    el.innerHTML = html;
-    return el.firstChild;
+  var el = document.createElement("div");
+  el.innerHTML = html;
+  return el.firstChild;
 }
 
 // Draw the donut segments
 function donutSegment(start, end, r, r0, color) {
-    if (end - start === 1) end -= 0.00001;
-    var a0 = 2 * Math.PI * (start - 0.25);
-    var a1 = 2 * Math.PI * (end - 0.25);
-    var x0 = Math.cos(a0),
-        y0 = Math.sin(a0);
-    var x1 = Math.cos(a1),
-        y1 = Math.sin(a1);
-    var largeArc = end - start > 0.5 ? 1 : 0;
+  if (end - start === 1) end -= 0.00001;
+  var a0 = 2 * Math.PI * (start - 0.25);
+  var a1 = 2 * Math.PI * (end - 0.25);
+  var x0 = Math.cos(a0),
+    y0 = Math.sin(a0);
+  var x1 = Math.cos(a1),
+    y1 = Math.sin(a1);
+  var largeArc = end - start > 0.5 ? 1 : 0;
 
-    return [
-        '<path d="M',
-        r + r0 * x0,
-        r + r0 * y0,
-        'L',
-        r + r * x0,
-        r + r * y0,
-        'A',
-        r,
-        r,
-        0,
-        largeArc,
-        1,
-        r + r * x1,
-        r + r * y1,
-        'L',
-        r + r0 * x1,
-        r + r0 * y1,
-        'A',
-        r0,
-        r0,
-        0,
-        largeArc,
-        0,
-        r + r0 * x0,
-        r + r0 * y0,
-        '" fill="' + color + '" />'
-    ].join(' ');
+  return [
+    '<path d="M',
+    r + r0 * x0,
+    r + r0 * y0,
+    "L",
+    r + r * x0,
+    r + r * y0,
+    "A",
+    r,
+    r,
+    0,
+    largeArc,
+    1,
+    r + r * x1,
+    r + r * y1,
+    "L",
+    r + r0 * x1,
+    r + r0 * y1,
+    "A",
+    r0,
+    r0,
+    0,
+    largeArc,
+    0,
+    r + r0 * x0,
+    r + r0 * y0,
+    '" fill="' + color + '" />',
+  ].join(" ");
 }
--- freshfoods/server/lib/util.js
+++ freshfoods/server/lib/util.js
@@ -1,42 +1,39 @@
 const cheerio = require("cheerio");
-const axios = require('axios');
-
-
 
 //Sample Entity Record Payload
 let co_record = {
-  "contact_id": "dummmy@example.com",
-  "contact_name": "N.A",
-  "ticket_id": 0,
-  "loc_lat": 0,
-  "loc_long": 0,
-  "loc_zipcode": 0,
-  "loc_locality": "",
-  "loc_area2": "",
-  "loc_area1": "",
-  "loc_country": "",
-  "cart_value": 0,
-  "near_del_cen": "",
-  "category": "",
-  "status": "0",
-  "items": "",
-  "cart_value_bucket": 0
+  contact_id: "dummmy@example.com",
+  contact_name: "N.A",
+  ticket_id: 0,
+  loc_lat: 0,
+  loc_long: 0,
+  loc_zipcode: 0,
+  loc_locality: "",
+  loc_area2: "",
+  loc_area1: "",
+  loc_country: "",
+  cart_value: 0,
+  near_del_cen: "",
+  category: "",
+  status: "0",
+  items: "",
+  cart_value_bucket: 0,
 };
 
 // From low to high
 function getCartValueCategory(cv) {
   switch (true) {
-    case (cv <= 100):
+    case cv <= 100:
       return 1;
-    case (cv > 100 && cv <= 250):
+    case cv > 100 && cv <= 250:
       return 2;
-    case (cv > 250 && cv <= 500):
+    case cv > 250 && cv <= 500:
       return 3;
-    case (cv > 500 && cv <= 750):
+    case cv > 500 && cv <= 750:
       return 4;
-    case (cv > 750 && cv <= 1000):
+    case cv > 750 && cv <= 1000:
       return 5;
-    case (cv > 1000):
+    case cv > 1000:
       return 6;
   }
 }
@@ -63,97 +60,95 @@ function parseStandardFields(data) {
     category: categ.includes("Dairy") ? "[M]" : "[F]",
     cart_value_bucket: getCartValueCategory(parseInt(cv)),
     status: "0",
-    items: items
-  }
+    items: items,
+  };
 }
 
 // Obtain structured data of the address components
-function reverseGeoCode(args, data) {
-  return new Promise((resolve, reject) => {
-    let t_cor = {};
-    axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-        params: {
-          key: args.iparams["gmp-api-key"],
-          latlng: String(data.loc_lat) + "," + String(data.loc_long)
-        }
-      })
-      .then(function (response) {
-        // Get locality
-        const ac = response.data.results[0].address_components;
-        for (i in ac) {
-          if (ac[i].types.includes("locality")) {
-            t_cor.loc_locality = ac[i].long_name;
-            break;
-          }
-        }
-        // Get loc_zipcode
-        for (i in ac) {
-          if (ac[i].types.includes("postal_code")) {
-            t_cor.loc_zipcode = parseInt(ac[i].long_name)
-            break;
-          }
-        }
-        // Get loc_area2
-        for (i in ac) {
-          if (ac[i].types.includes("administrative_area_level_2")) {
-            t_cor.loc_area2 = ac[i].long_name
-            break;
-          }
-        }
+async function reverseGeoCode(args, data) {
+  let t_cor = {};
+  const key = args.iparams["gmp-api-key"];
+  const latlng = String(data.loc_lat) + "," + String(data.loc_long);
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?key=${key}&latlng=${latlng}`;
+  let response;
 
-        // Get loc_area1
-        for (i in ac) {
-          if (ac[i].types.includes("administrative_area_level_1")) {
-            t_cor.loc_area1 = ac[i].long_name
-            break;
-          }
-        }
+  try {
+    response = await $request.get(url);
+  } catch (e) {
+    throw e;
+  }
 
-        // Get country
-        for (i in ac) {
-          if (ac[i].types.includes("country")) {
-            t_cor.loc_country = ac[i].long_name
-            break;
-          }
-        }
-        Object.assign(data, t_cor);
-        resolve(data);
-      })
-      .catch(function (error) {
-        reject(error);
-      })
-  });
+  // Get locality
+  const ac = response.data.results[0].address_components;
+  for (const i in ac) {
+    if (ac[i].types.includes("locality")) {
+      t_cor.loc_locality = ac[i].long_name;
+      break;
+    }
+  }
+  // Get loc_zipcode
+  for (const i in ac) {
+    if (ac[i].types.includes("postal_code")) {
+      t_cor.loc_zipcode = parseInt(ac[i].long_name);
+      break;
+    }
+  }
+  // Get loc_area2
+  for (const i in ac) {
+    if (ac[i].types.includes("administrative_area_level_2")) {
+      t_cor.loc_area2 = ac[i].long_name;
+      break;
+    }
+  }
+
+  // Get loc_area1
+  for (const i in ac) {
+    if (ac[i].types.includes("administrative_area_level_1")) {
+      t_cor.loc_area1 = ac[i].long_name;
+      break;
+    }
+  }
+
+  // Get country
+  for (const i in ac) {
+    if (ac[i].types.includes("country")) {
+      t_cor.loc_country = ac[i].long_name;
+      break;
+    }
+  }
+  Object.assign(data, t_cor);
+  return data;
 }
 
 // Use Google Maps API to obtain the distance between delivery hubs
-function computeDistanceMatrix(args, data) {
-  return new Promise((resolve, reject) => {
-    axios.get('https://maps.googleapis.com/maps/api/distancematrix/json', {
-        params: {
-          units: 'metric',
-          origins: String(data.loc_lat) + "," + String(data.loc_long),
-          destinations: "37.564457,-122.331298|37.42097, -122.130387|37.320918,-122.036744", // Delivery hubs are hardcoded, supposed to be separate entities on their own
-          key: args.iparams["gmp-api-key"]
-        }
-      })
-      .then(function (response) {
-        Object.assign(data, {
-          near_del_cen: JSON.stringify(response.data.rows[0].elements)
-        })
-        resolve(data);
-      })
-      .catch(function (error) {
-        reject(error);
-      })
+async function computeDistanceMatrix(args, data) {
+  const units = "metric";
+  const origins = String(data.loc_lat) + "," + String(data.loc_long);
+  const dest =
+    "37.564457,-122.331298|37.42097, -122.130387|37.320918,-122.036744"; // Delivery hubs are hardcoded, supposed to be separate entities on their own
+  const key = args.iparams["gmp-api-key"];
+  const url = `https://maps.googleapis.com/maps/api/distancematrix/json?key=${key}&units=${units}&origins=${origins}&destinations=${dest}`;
+  let response;
+
+  try {
+    response = await $request.get(url);
+  } catch (e) {
+    throw e;
+  }
+
+  Object.assign(data, {
+    near_del_cen: JSON.stringify(response.data.rows[0].elements),
   });
+  return data;
 }
+
 // Creates the C.O record
 function createDeliveryRequest(args, data, ent_name) {
   const entity = $db.entity({
-    version: 'v1'
-  });;
+    version: "v1",
+  });
   const delivery_requests = entity.get(ent_name);
-  console.info(data);
+  console.log(data);
   return delivery_requests.create(data);
 }
 
@@ -161,9 +156,9 @@ function createDeliveryRequest(args, data, ent_name) {
 function logIt(u, p, m) {
   try {
     let msg = JSON.stringify(m);
-    console.info("Logging : ", msg);
+    console.log("Logging : ", msg);
     $request.post(`${u}/${p}`, {
-      body: msg
+      body: msg,
     });
   } catch (error) {
     console.error("Oops, Something went wrong!");
@@ -177,5 +172,5 @@ exports = {
   computeDistanceMatrix,
   reverseGeoCode,
   createDeliveryRequest,
-  logIt
+  logIt,
 };
