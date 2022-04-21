6bb055fabd0e7a8ac023a687fc96b18ffe59d356
Kaustav Das Modak <kaustav.dasmodak@freshworks.com>
2022-04-21 09:34:31 +0530

    (wip): Get entities and iparams in place
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@freshworks.com>

--- freshservice/employee-sync/server/server.js
+++ freshservice/employee-sync/server/server.js
@@ -1,3 +1,34 @@
-exports = {
+const { Freshteam } = require("@freshworks/api-sdk");
 
+exports = {
+  /**
+   * Handler invoked with `onAppInstall` event. Creates scheduled function to run every 30 minutes.
+   */
+  async install() {
+    try {
+      await $schedule.create({
+        name: "freshteam_employee_sync",
+        data: {},
+        schedule_at: new Date(Date.now() + 10000).toISOString(), // 10 seconds in the future
+        repeat: {
+          time_unit: "minutes",
+          frequency: 30,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    renderData();
+  },
+
+  /**
+   * Handler invoked `onScheduledEvent`
+   * @param {object} args
+   */
+  async sync_records(args) {
+    const domain = `${args.iparams.freshteam_domain}.freshteam.com`
+    const ft = new Freshteam(domain, args.iparams.freshteam_api_key);
+
+
+  },
 };
