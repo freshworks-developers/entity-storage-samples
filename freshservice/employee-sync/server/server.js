9d21c83c2a28b84f0405f374683835a08e941784
Kaustav Das Modak <kaustav.dasmodak@freshworks.com>
2022-04-23 15:36:51 +0530

    (wip): Sync data
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@freshworks.com>

--- freshservice/employee-sync/server/server.js
+++ freshservice/employee-sync/server/server.js
@@ -1,23 +1,84 @@
 const { Freshteam } = require("@freshworks/api-sdk");
 
+/**
+ * Get employees sorted by employee_id in ascending order
+ *
+ * @param {string} domain
+ * @param {string} apiKey
+ * @param {number} page
+ * @returns
+ */
+function getEmployees(domain, apiKey, page = 1) {
+  const ft = new Freshteam(domain, apiKey);
+
+  return ft.employees.list({
+    sort: "employee_id",
+    sort_type: "asc",
+    page,
+  });
+}
+
+async function getLastPage() {
+  try {
+    const { page } = await $db.get("sync_state");
+    return page;
+  } catch (err) {
+    console.log(`Error getting last page: ${err.message}`);
+    return 1;
+  }
+}
+
+function incrementLastPage() {
+  return $db.update("sync_state", "increment", { page: 1 });
+}
+
+function resetLastPage() {
+  return $db.set("sync_state", { page: 1 }, { setIf: "exist" });
+}
+
+async function syncRecords(domain, apiKey) {
+  try {
+    const page = await getLastPage();
+    const employees = await getEmployees(domain, apiKey, page);
+    console.log("page", page);
+    console.log(employees.length);
+    if (employees.length) {
+      await incrementLastPage();
+    } else {
+      await resetLastPage();
+    }
+    return employees.length;
+  } catch (err) {
+    console.log(`Error syncing: ${err.message}`);
+    return 0;
+  }
+}
+
+function createSchedule() {
+  // Create a schedule to run every hour, starting 5 minutes from app install
+  return $schedule.create({
+    name: "freshteam_employee_sync",
+    data: {},
+    schedule_at: new Date(Date.now() + 5 * 60 * 1000).toISOString(), // 5 minutes in the future
+    repeat: {
+      time_unit: "hours",
+      frequency: 1,
+    },
+  });
+}
+
 exports = {
   /**
    * Handler invoked with `onAppInstall` event. Creates scheduled function to run every 30 minutes.
    */
-  async install() {
+  async onAppInstall() {
     try {
-      await $schedule.create({
-        name: "freshteam_employee_sync",
-        data: {},
-        schedule_at: new Date(Date.now() + 10000).toISOString(), // 10 seconds in the future
-        repeat: {
-          time_unit: "minutes",
-          frequency: 30,
-        },
-      });
+      await createSchedule();
     } catch (err) {
-      console.log(err);
+      console.log(`Error creating schedule: ${err.message}`);
+      renderData(err); // Prevent install completion if creating schedule fails
     }
+    // All good, let app install reach completion
     renderData();
   },
 
@@ -25,10 +86,9 @@ exports = {
    * Handler invoked `onScheduledEvent`
    * @param {object} args
    */
-  async sync_records(args) {
-    const domain = `${args.iparams.freshteam_domain}.freshteam.com`
-    const ft = new Freshteam(domain, args.iparams.freshteam_api_key);
-
-
+  async onScheduledEvent(args) {
+    const domain = `${args.iparams.freshteam_domain}.freshteam.com`;
+    const apiKey = args.iparams.freshteam_api_key;
+    await syncRecords(domain, apiKey);
   },
 };
